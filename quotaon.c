4d93b438f3e5f2349a3b7826fa2387d25f9de3c1
Ondrej Vasik <ovasik@redhat.com>
2011-01-11 23:00:37 +0100

    Fix help text for quotaoff
    Signed-off-by: Jan Kara <jack@suse.cz>

--- quotaon.c
+++ quotaon.c
@@ -65,7 +65,7 @@ static void usage(void)
 {
 	errstr(_("Usage:\n\t%s [-guvp] [-F quotaformat] [-x state] -a\n\
 \t%s [-guvp] [-F quotaformat] [-x state] filesys ...\n\n\
--a, --all                turn quotas on for all filesystems\n\
+-a, --all                turn quotas %s for all filesystems\n\
 -f, --off                turn quotas off\n\
 -u, --user               operate on user quotas\n\
 -g, --group              operate on group quotas\n\
@@ -74,7 +74,8 @@ static void usage(void)
 -F, --format=formatname  operate on specific quota format\n\
 -v, --verbose            print more messages\n\
 -h, --help               display this help text and exit\n\
--V, --version            display version information and exit\n"), progname, progname);
+-V, --version            display version information and exit\n"),
+ progname, progname, strcmp(progname, "quotaon") ? _("off") : _("on"));
 	exit(1);
 }
 
