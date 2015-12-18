618a2a3a9de313dd5d7b783a232c8147cf23ae79
Jan Kara <jack@suse.cz>
2015-12-18 12:57:34 +0100

    Update release script to work with new build system
    Signed-off-by: Jan Kara <jack@suse.cz>

--- release.sh
+++ release.sh
@@ -9,10 +9,12 @@ VERSION=$1
 CURVERSION=`git tag | tail -1`
 CURVERSION=${CURVERSION:1}
 
+MAJOR=${VERSION%.*}
+MINOR=${VERSION#*.}
+
 # Update version in configure script
-sed -i -e 's/\[PACKAGE_VERSION\], \[".*"\],/\[PACKAGE_VERSION\], \["'$VERSION'"\],/' configure.in
-autoheader
-autoconf
+sed -i -e 's/\[quota_version_major\],\[.*\]/\[quota_version_major\],\['$MAJOR'\]/' configure.ac
+sed -i -e 's/\[quota_version_minor\],\[.*\]/\[quota_version_minor\],\['$MINOR'\]/' configure.ac
 
 echo "Changes in quota-tools from $CURVERSION to $VERSION" >Changelog.new
 git log --pretty="* %s (%an)" v$CURVERSION.. >>Changelog.new
@@ -20,11 +22,9 @@ echo "" >>Changelog.new
 cat Changelog >>Changelog.new
 mv Changelog.new Changelog
 
-git add Changelog configure.in
+git add Changelog configure.ac
 git commit -s -m "Release quota-tools $VERSION"
 git tag v$VERSION
 
 # Create tarball
-make realclean
-cd ..
-tar czvf quota-$VERSION.tar.gz --exclude .git --exclude autom4te.cache quota-tools/
+make dist-gzip
