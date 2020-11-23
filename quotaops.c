sailfishos-mirror/quota

commit 7942290a0a6230fcc56eae42bf436e6eeca88777

quotaops: Do not leak dquot structures on failure
Some error paths in getprivs() were not properly freeing already fetched
structures. Free them properly in case of error.

Signed-off-by: Jan Kara <jack@suse.cz>

quotaops.c
@@ -80,7 +80,7 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 				if (euid != id && euid != 0) {
 					uid2user(id, name);
 					errstr(_("%s (uid %d): Permission denied\n"), name, id);
-					return (struct dquot *)NULL;
+					goto out_err;
 				}
 				break;
 			case GRPQUOTA:
@@ -92,7 +92,7 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 					if (!gidsetp) {
 						gid2group(id, name);
 						errstr(_("%s (gid %d): gid set allocation (%d): %s\n"), name, id, ngroups, strerror(errno));
-						return (struct dquot *)NULL;
+						goto out_err;
 					}
 				}
 				else
@@ -103,7 +103,7 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 						free(gidsetp);
 					gid2group(id, name);
 					errstr(_("%s (gid %d): error while trying getgroups(): %s\n"), name, id, strerror(errno));
-					return (struct dquot *)NULL;
+					goto out_err;
 				}
 
 				for (j = 0; j < ngroups; j++)
@@ -115,7 +115,7 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 					gid2group(id, name);
 					errstr(_("%s (gid %d): Permission denied\n"),
 						name, id);
-					return (struct dquot *)NULL;
+					goto out_err;
 				}
 				break;
 			default:
@@ -134,6 +134,7 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 			id2name(id, handles[i]->qh_type, name);
 			errstr(_("error while getting quota from %s for %s (id %u): %s\n"),
 				handles[i]->qh_quotadev, name, id, strerror(olderrno));
+out_err:
 			freeprivs(qhead);
 			return NULL;
 		}

commit 8a3321627bb9215ed40f8f7036a1e86bcaf10ad1

quotaops: fix compilation warning
When quota is configured using --enable-werror --disable-bsd_behaviour,
the compilation fails with the following diagnostics:

quotaops.c: In function 'getprivs':
quotaops.c:143:1: error: label 'out_err' defined but not used [-Werror=unused-label]

Fixes: 7942290a ("quotaops: Do not leak dquot structures on failure")
Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
Signed-off-by: Jan Kara <jack@suse.cz>

quotaops.c
@@ -140,7 +140,9 @@ struct dquot *getprivs(qid_t id, struct quota_handle **handles, int ignore_noquo
 			id2name(id, handles[i]->qh_type, name);
 			errstr(_("error while getting quota from %s for %s (id %u): %s\n"),
 				handles[i]->qh_quotadev, name, id, estr);
+#if defined(BSD_BEHAVIOUR)
 out_err:
+#endif
 			freeprivs(qhead);
 			return NULL;
 		}